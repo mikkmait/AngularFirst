a1849d4f006dcda219c3e28eb4e6d318f28de59f
mikkmait <mikkmait@gmail.com>
2023-03-27 23:35:05 +0300

    Playing around with hover buttons.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = '**** **** STACK';
-}
+
+  handleRed(element: HTMLElement) {
+    element.style.backgroundColor = 'rgba(255, 0, 63, 100%)';
+  }
+  handleBlue(element: HTMLElement) {
+    element.style.backgroundColor = 'rgba(63, 0, 255, 100%)';
+  }
+};
